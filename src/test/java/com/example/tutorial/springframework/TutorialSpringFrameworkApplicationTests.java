bcdf03b64e0fb77aa168808528ad6fbd54558412
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-10-30 14:44:17 +0900

    first commit

---
+++ src/test/java/com/example/tutorial/springframework/TutorialSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorial.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
